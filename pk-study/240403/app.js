9e96aaef0111506dcc3d0622f71a748b9d3161e7
박준형 <108733795+krapli441@users.noreply.github.com>
2024-04-03 17:18:55 +0900

    jwt 생성하는 예제 코드 작성

---
+++ pk-study/240403/app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const badyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+// ! JWT 비밀 키
+const SECRET_KEY = crypto.randomBytes(256).toString("hex");
+
+app.use(bodyParser.json());
+
+// ? 간단한 사용자 데이터베이스 대용
+const users = [
+  {
+    id: "1",
+    username: "user1",
+    password: "1234",
+  },
+  {
+    id: "2",
+    username: "user2",
+    password: "5678",
+  },
+];
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  // 사용자 검증
+  const users = users.find(
+    (u) => u.username === username && u.password === password
+  );
+
+  if (users) {
+    // JWT 생성
+    const token = jwt.sign(
+      { userId: user.id, username: user.username },
+      SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+    res.json({ token });
+  } else {
+    res.status(401).send("Username or password is incorrect");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
