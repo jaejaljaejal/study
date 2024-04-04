c486823da2ce922bd200008e33dd4e20af958343
박준형 <108733795+krapli441@users.noreply.github.com>
2024-04-04 18:51:35 +0900

    로그인시 check로 넘어가도록 함

--- pk-study/240403/app.js
+++ pk-study/240403/app.js
@@ -58,6 +58,16 @@ app.post("/login", (req, res) => {
   }
 });
 
+app.post("/verify", (req, res) => {
+  const token = req.headers.authorization.split(" ")[1]; // 'Bearer TOKEN_STRING'에서 토큰 추출
+  jwt.verify(token, SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.json({ message: "유효하지 않은 jwt 토큰입니다" });
+    }
+    res.json({ message: "유효한 jwt 토큰입니다" });
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
